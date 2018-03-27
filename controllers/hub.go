688458af8e388849b1ddfb6dc9af48cf9dceee04
lbi <452381072@qq.com>
2018-03-27 16:46:54 +0800

    更改目录

--- controllers/hub.go
+++ controllers/hub.go
@@ -6,10 +6,7 @@ import (
 	"net/http"
 	"bytes"
 	"os/exec"
-	"text/template"
 	"log"
-	"time"
-	"encoding/json"
 )
 
 type Webhook struct {
@@ -42,8 +39,6 @@ type TemplateData struct {
 	//Params   string
 }
 
-var httpClient = &http.Client{Timeout: 10 * time.Second}
-var deployScript = template.Must(template.ParseFiles("deploy.sh"))
 
 var repoPathMapping = map[string]string{
 	"ssoserver": "ssoserver",
@@ -83,50 +78,32 @@ func Deploy(c *gin.Context) {
 		path = webhook.Repository.Name
 	}
 
-	info := TemplateData{
-		Path: "/opt/compose/" + path,
-	}
+	path = "/opt/compose/" + path
+	fmt.Println(path)
 
-	buff := new(bytes.Buffer)
-	buff.Reset()
-	deployScript.Execute(buff, info)
+	cmd := exec.Command("cd", path)
+	fmt.Println("cd " + path)
+	cmd.Run()
 
-	fmt.Println(buff.String())
-
-	cmd := exec.Command("bash", "-c", buff.String())
-	stdouterr, err := cmd.CombinedOutput()
+	cmd = exec.Command("docker-compose", "pull")
+	fmt.Println("docker-compose pull")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
 	if err != nil {
-		log.Printf("script error: %s", err.Error())
-		fmt.Println(stdouterr)
-		//sendCallback(w, payload.CallbackURL, false, "script error: "+err.Error())
-		//return
-		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "msg": "script error: " + err.Error()})
-		return
+		log.Fatal(err)
 	}
+	fmt.Printf("GOGOGO: %q\n", out.String())
+
+	cmd = exec.Command("docker-compose", "up", "-d")
+	fmt.Println("docker-compose up -d")
+	var out2 bytes.Buffer
+	cmd.Stdout = &out2
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("GOGOGO: %q\n", out2.String())
 
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
-
-}
-
-func sendCallback(w http.ResponseWriter, url string, success bool, description string) {
-	body := Callback{
-		State:       "failure",
-		Context:     "Webhook deploy server",
-		Description: description,
-	}
-	if len(body.Description) > 255 {
-		body.Description = body.Description[0:255]
-	}
-	if success {
-		body.State = "success"
-	}
-	buff := new(bytes.Buffer)
-	json.NewEncoder(buff).Encode(body)
-	res, err := httpClient.Post(url, "application/json; charset=utf-8", buff)
-	if err != nil || res.StatusCode != 200 {
-		log.Printf("invalid callback: %d", res.StatusCode)
-		//writeError(w, "invalid callback", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprint(w, "{\"ok\":true}")
 }
